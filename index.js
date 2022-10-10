dd1cfbfbde26bf92675c92a1741a7e2396512e03
Kevin Price <collegekevin@gmail.com>
2022-10-10 16:13:08 -0700

    commit tree ad4b6a2fb06d2c700603527a059315e2b91b3cbd

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) 
+    return "This one is on me!"
+  else if (feet <= 2000)
+    return "That will be twenty bucks."
+  else if (feet <= 2500)
+    return "I will gladly take your thirty bucks." 
+  else if (feet >2500)
+    return "No can do."
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  return destination === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(comment){
+  switch (comment) {
+    case 'generous' : 
+      return "Thank you so much.";
+    case 'not as generous' :
+      return "Thank you.";
+    default :
+      return "Bye.";
+
+
+  }
   // Write your code here!
 }
